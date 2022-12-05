140784a46fa27d58d58ac99c71f3f172716359dd
Pattie30 <87343336+Pattie30@users.noreply.github.com>
2022-12-05 15:02:28 -0500

    first commit todolist

---
+++ src/TodoList.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import NewTodoForm from './NewTodoForm';
+import Todo from './Todo';
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [{ task: 'Walk the fish'}, { task: 'groom the chickens'}]
+        }; 
+        this.create = this.create.bind(this);
+        this.remove =this.remove.bind(this);
+    }
+    create(newTodo){
+        this.setState({
+            todos: [...this.state.todos, newTodo]
+        });
+        
+      }
+      remove(id) {
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        })
+      }
+    render() {
+        const todos = this.state.todos.map(todo => {
+            return (
+            <Todo 
+            key={todo.id} 
+            id={todo.id} 
+            task ={todo.task} 
+            removeTodo={this.remove}
+
+            />
+        );
+    });
+        return(
+            
+            <div>
+            <h1>Todo List!</h1>
+               
+                
+                <NewTodoForm createTodo={this.create} />
+
+                <ul>{todos}</ul>
+
+            </div>
+        );
+    }
+}
+    
+
+export default TodoList;
